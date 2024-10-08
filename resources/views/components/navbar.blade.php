379fa75bfccf78a4978711a650658b1b4627695c
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-10-09 05:06:37 +0800

    finishing navbar, prepare for hero

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,17 @@
 <div>
     <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
-    <nav class="flex justify-between container mx-auto py-3 px-4 bg-cyan-400 ">
-        
+    <nav class="fixed bg-cyan-200 w-full ">
+        <div class="flex justify-between container mx-auto py-3 px-4 left-auto ">
+            <div class="flex items-center space-x-4">
+                <i class="fa-solid fa-tree text-white text-3xl"></i>
+                <p class="text-base text-white font-semibold inline">Taman Kita</p>
+            </div>
+            <div class="Flex space-x-8">
+                <a href="#" class="font-semibold text-white">Home</a>
+                <a href="#" class="font-semibold text-white">Facility</a>
+                <a href="#" class="font-semibold text-white">Gallery</a>
+                <a href="#" class="font-semibold text-white">Ticket</a>
+            </div>
+        </div>
     </nav>
 </div>
--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.app')
 @section('content')
-    
+<header class="w-full h-screen bg-black">
+   <div>
+      
+   </div>
+</header>
 @endsection
