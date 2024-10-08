3c0bb81615a834040e0864c937a09c2d07d4f77e
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-10-09 02:24:20 +0800

    adding components

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+    <nav class="flex justify-between container mx-auto py-3 px-4 bg-cyan-400 ">
+        
+    </nav>
+</div>
